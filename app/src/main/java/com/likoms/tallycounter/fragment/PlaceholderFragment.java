e2f2daf8b16d2a687d2830cae1226869e30bcbe3
likoms <toribosh@gmail.com>
2015-02-21 18:52:11 +0100

    Problem with alert

--- app/src/main/java/com/likoms/tallycounter/fragment/PlaceholderFragment.java
+++ app/src/main/java/com/likoms/tallycounter/fragment/PlaceholderFragment.java
@@ -1,5 +1,7 @@
 package com.likoms.tallycounter.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,6 +20,7 @@ public class PlaceholderFragment extends Fragment {
     private TextView counter;
     private Button nextButton;
     private Button resetButton;
+    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
     public PlaceholderFragment() {
     }
@@ -41,16 +44,54 @@ public class PlaceholderFragment extends Fragment {
                 counter.setText((index+1)+"");
             }
         });
+        //Dialog
+        this.builder.setMessage(R.string.confirm_dialog)
+                .setPositiveButton(R.string.yes_option_dialog, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        counter.setText("0");
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton(R.string.cancel_option_dialog, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        //End of dialog
         this.resetButton = (Button) rootView.findViewById(R.id.reset);
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODo show dialog to user if he is sure
-                counter.setText("0");
+                // Create the AlertDialog object and return it
+                AlertDialog alert = builder.create();
+                alert.show();
+                //counter.setText("0");
             }
         });
+
+
+
         return rootView;
     }
+  /* public void showMsgDialog() {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.confirm_dialog)
+                .setPositiveButton(R.string.yes_option_dialog, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        counter.setText("0");
+                    }
+                })
+                .setNegativeButton(R.string.cancel_option_dialog, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+       AlertDialog alert = builder.create();
+       alert.show();
+    }*/
 
 
 }
